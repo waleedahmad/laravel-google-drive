09c5fca506405fea1d680ffff557ebb866f9d5d5
Waleed Ahmad <waleedgplus@gmail.com>
2019-05-18 17:25:48 +0500

    Added: upload and list file link on index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -72,22 +72,22 @@
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
                     @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Google Drive / Laravel
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    @auth
+                        <a href="/drive">List Google Drive Folders & Files</a>
+                        <a href="/drive/upload">Upload Files</a>
+                    @else
+                        <a href="/login/google">Login With Google</a>
+                    @endauth
                 </div>
             </div>
         </div>
